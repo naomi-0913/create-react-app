17e850082c83639d64aee88521862f84766e393c
naomi-0913 <upworld409019@gmail.com>
2020-12-11 22:52:05 +0900

    create setState()

--- src/Article.jsx
+++ src/Article.jsx
@@ -4,6 +4,8 @@ const Article = (props) => {
   return (
     <>
       <h2>{props.title}</h2>
+      <label htmlFor="check" >公開状態：</label>
+      <input type="checkBox" checked={props.isPublished} id="check" onClick={() => props.toggle()}/>
     </>
   )
 }
--- src/Blog.jsx
+++ src/Blog.jsx
@@ -4,13 +4,22 @@ import Article from './Article';
 class Blog extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isPublished: false,
+      order: 1
+    }
   }
+
+  togglePublished = () => {
+    this.setState({
+      isPublished: !this.state.isPublished
+    })
+  };
+
   render () {
     return (
       <>
-        <Article title={"React"}/>
-        <Article title={"Component"}/>
-        <Article title={"再利用"}/>
+        <Article title={"React"} isPublished={this.state.isPublished} toggle={() => this.togglePublished()}/>
       </>
     )
   }
