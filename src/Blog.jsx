8412ad3345867aedbfa485c27400b55441807dc2
naomi-0913 <upworld409019@gmail.com>
2020-12-12 23:03:39 +0900

    use setEffect() of React Hooks

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -4,24 +4,7 @@ import * as FooBar from './components/FooBar';
 import Hoge from './components/Hoge';
 
 const Blog= () => {
-  // componentDidMount() {
-  //   document.getElementById("counter").addEventListener('click', this.countUp)
-  // }
 
-  // componentDidUpdate() {
-  //   if (this.state.count >= 10 ) {
-  //     this.setState({count: 0})
-  //   }
-  // }
-
-  // componentWillUnmount() {
-  //   document.getElementById("counter").removeEventListener('click', this.countUp)
-  // }
-
-
-  // countUp = () => {
-  //   this.setState ({count: this.state.count + 1 })
-  // }
 
   return (
     <>
