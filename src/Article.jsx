455f493f085f4d877a79e1b63a845eb37327682a
naomi-0913 <upworld409019@gmail.com>
2020-12-12 22:15:37 +0900

    use setState() of React Hooks

--- src/Article.jsx
+++ src/Article.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import LikeButton from './LikeButton'
+import React, {useState} from 'react';
+import LikeButton from './LikeButton';
 
 const Article = (props) => {
+  const [isPublished, togglePublished] = useState(false);
+
   return (
     <>
       <h2>{props.title}</h2>
       <label htmlFor="check" >公開状態：</label>
-      <input type="checkBox" checked={props.isPublished} id="check" onClick={() => props.toggle()}/>
-      <LikeButton count={props.count}></LikeButton>
+      <input type="checkBox" checked={isPublished} id="check" onClick={() => togglePublished(!isPublished)}/>
+      {/* <LikeButton count={props.count}></LikeButton> */}
     </>
   )
 }
